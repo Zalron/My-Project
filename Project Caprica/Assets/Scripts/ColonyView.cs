552aaa77ff3189bffd059a9a9ff02a91ce6d1cf6
Zalron <33308793+Zalron@users.noreply.github.com>
2019-01-30 17:31:05 +1100

    Added Project Caprica

---
+++ Project Caprica/Assets/Scripts/ColonyView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColonyView : MonoBehaviour
+{
+    //Get called every time we become active (including possibly on scene load)
+    private void OnEnable()
+    {
+        //Update our data elements based on the selected colony
+    }
+
+    public object Colony;
+
+    // Start is called before the first frame update. Use this for one-time initialization, AFTER OnEnable is called
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
